c2be084a97316d6e169eb9a300fb5e13bab13ffd
gianhing <gianhing@qq.com>
2024-04-01 17:54:38 +0800

    feat(rpc): 添加系统SPI

--- rpc-core/src/main/java/com/example/rpc/proxy/ServiceProxy.java
+++ rpc-core/src/main/java/com/example/rpc/proxy/ServiceProxy.java
@@ -13,6 +13,7 @@ import com.example.rpc.serializer.Serializer;
 import java.io.IOException;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
+import java.util.ServiceLoader;
 
 /**
  * 服务代理类：由代理对象完成请求和响应
@@ -21,7 +22,15 @@ public class ServiceProxy implements InvocationHandler {
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
         // 指定序列化器
-        Serializer serializer = new JdkSerializer();
+        Serializer serializer = null;
+        ServiceLoader<Serializer> serializerLoader = ServiceLoader.load(Serializer.class);
+        for (Serializer item : serializerLoader) {
+            serializer = item;
+        }
+        if (serializer == null) {
+            serializer = new JdkSerializer();
+        }
+
         // 构造请求
         RpcRequest rpcRequest = RpcRequest.builder()
                 .serviceName(method.getDeclaringClass().getName())
